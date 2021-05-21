3f6b6c3dfbd8939ce58a9007484ff1a74542cffc
Jacopo Zecchi <jacopo.zecchi@gmail.com>
2021-05-21 19:50:22 +0200

    added tests

--- test/ImageTest.cpp
+++ test/ImageTest.cpp
@@ -4,7 +4,7 @@
 #include "../Image.h"
 #include "../utils.h"
 
-class ImageTest: public ::testing::Test{
+class ImageTest : public ::testing::Test {
 
 protected:
     Image solid_pink;
@@ -14,47 +14,59 @@ protected:
     Image not_uni;
     Image mono_green;
 
-    ImageTest():
-        solid_pink(PATH + "3x2_solid_pink.jpg"),
-        solid_green(PATH + "3x3_solid_green.jpg"),
-        alpha_yellow(PATH + "3x4_alpha_yellow.png"),
-        big(PATH + "big.jpg"),
-        not_uni(PATH + "not_uni.png"),
-        mono_green(PATH + "mono_green.png")
-    {}
+    ImageTest() :
+            solid_pink(PATH + "3x2_solid_pink.jpg"),
+            solid_green(PATH + "3x3_solid_green.jpg"),
+            alpha_yellow(PATH + "3x4_alpha_yellow.png"),
+            big(PATH + "big.jpg"),
+            not_uni(PATH + "not_uni.png"),
+            mono_green(PATH + "mono_green.png") {}
 
 };
 
-TEST_F(ImageTest, Path){
+TEST_F(ImageTest, Ctor) {
+    ASSERT_EQ(solid_pink.getSize(), 6);
+    Pixel *data = solid_pink.getDeepData();
+
+    Pixel tempData[6] = {
+            Pixel(255, 10, 227, 255), Pixel(255, 10, 227, 255), Pixel(255, 10, 227, 255),
+            Pixel(255, 10, 227, 255), Pixel(255, 10, 227, 255), Pixel(255, 10, 227, 255)
+    };
+
+    for (int i = 0; i < 6; i++)
+        EXPECT_EQ(data[i], tempData[i]);
+}
+
+TEST_F(ImageTest, Path) {
     EXPECT_EQ(solid_pink.getPath(), "../../test/test-images/3x2_solid_pink.jpg");
     EXPECT_EQ(solid_green.getPath(), "../../test/test-images/3x3_solid_green.jpg");
     EXPECT_EQ(alpha_yellow.getPath(), "../../test/test-images/3x4_alpha_yellow.png");
 }
 
-TEST_F(ImageTest, Filename){
+TEST_F(ImageTest, Filename) {
 
     EXPECT_EQ(solid_pink.getFilename(), "3x2_solid_pink.jpg");
     EXPECT_EQ(solid_green.getFilename(), "3x3_solid_green.jpg");
     EXPECT_EQ(alpha_yellow.getFilename(), "3x4_alpha_yellow.png");
 }
 
-TEST_F(ImageTest, Size){
+TEST_F(ImageTest, Size) {
     EXPECT_EQ(solid_pink.getSize(), 6);
     EXPECT_EQ(solid_green.getSize(), 9);
     EXPECT_EQ(alpha_yellow.getSize(), 12);
-    EXPECT_EQ(big.getSize(), 2560*1440);
+    EXPECT_EQ(big.getSize(), 2560 * 1440);
 }
 
-void calculateCheckGrayScale(Image& img){
-    Pixel* tempData;
-    Pixel* data;
+void calculateCheckGrayScale(Image &img) {
+    Pixel *tempData;
+    Pixel *data;
 
     //test img
     tempData = img.getDeepData();
     img.grayScaleOptimized();
     data = img.getDeepData();
 
-    for(int i = 0; i<img.getSize(); i++){
+    for (int i = 0; i < img.getSize(); i++) {
         int gray = 0.2126 * tempData[i].getR() + 0.7125 * tempData[i].getG() + 0.0722 * tempData[i].getB();
         tempData[i] = Pixel(gray, gray, gray, tempData[i].getA());
 
@@ -65,21 +77,21 @@ void calculateCheckGrayScale(Image& img){
     delete data;
 }
 
-TEST_F(ImageTest, GrayScale){
+TEST_F(ImageTest, GrayScale) {
 
     calculateCheckGrayScale(solid_pink);
     calculateCheckGrayScale(alpha_yellow);
 }
 
-TEST_F(ImageTest, ColorMask){
+TEST_F(ImageTest, ColorMask) {
 
     //
 }
 
-TEST_F(ImageTest, FlipX){
+TEST_F(ImageTest, FlipX) {
 
     //not_uni.png data flipped X
-    Pixel tempData_not_uni[3*2] = {
+    Pixel tempData_not_uni[3 * 2] = {
             Pixel(161, 255, 20, 255), Pixel(243, 94, 207, 255),
             Pixel(255, 0, 0, 255), Pixel(25, 61, 197, 255),
             Pixel(87, 42, 143, 255), Pixel(88, 255, 218, 255)};
@@ -88,11 +100,11 @@ TEST_F(ImageTest, FlipX){
 
     not_uni.flip90Dx();
     not_uni.flipX();
-    Pixel* data = not_uni.getDeepData();
+    Pixel *data = not_uni.getDeepData();
 
     ASSERT_EQ(not_uni.getSize(), 6);
 
-    for(int i=0; i<6; i++)
+    for (int i = 0; i < 6; i++)
         EXPECT_EQ(data[i], tempData_not_uni[i]);
 
     ASSERT_EQ(mono_green.getSize(), 1);
@@ -106,21 +118,21 @@ TEST_F(ImageTest, FlipX){
 
 }
 
-TEST_F(ImageTest, FlipY){
+TEST_F(ImageTest, FlipY) {
 
     //not_uni.png
-    Pixel tempData_not_uni[2*3] = {
+    Pixel tempData_not_uni[2 * 3] = {
             Pixel(243, 94, 207, 255), Pixel(25, 61, 197, 255), Pixel(88, 255, 218, 255),
-            Pixel(161, 255, 20, 255),Pixel(255, 0, 0, 255), Pixel(87, 42, 143, 255) };
+            Pixel(161, 255, 20, 255), Pixel(255, 0, 0, 255), Pixel(87, 42, 143, 255)};
 
     Pixel tempData_mono_green[1] = {Pixel(66, 133, 7, 255)};
 
     not_uni.flipY();
-    Pixel* data = not_uni.getDeepData();
+    Pixel *data = not_uni.getDeepData();
 
     ASSERT_EQ(not_uni.getSize(), 6);
 
-    for(int i=0; i<6; i++)
+    for (int i = 0; i < 6; i++)
         EXPECT_EQ(data[i], tempData_not_uni[i]);
 
     delete data;
@@ -131,8 +143,8 @@ TEST_F(ImageTest, FlipY){
 
 }
 
-TEST_F(ImageTest, Flip90DX){
-    Pixel tempData_not_uni[3*2] = {
+TEST_F(ImageTest, Flip90DX) {
+    Pixel tempData_not_uni[3 * 2] = {
             Pixel(243, 94, 207, 255), Pixel(161, 255, 20, 255),
             Pixel(25, 61, 197, 255), Pixel(255, 0, 0, 255),
             Pixel(88, 255, 218, 255), Pixel(87, 42, 143, 255)};
@@ -140,11 +152,11 @@ TEST_F(ImageTest, Flip90DX){
     Pixel tempData_mono_green[1] = {Pixel(66, 133, 7, 255)};
 
     not_uni.flip90Dx();
-    Pixel* data = not_uni.getDeepData();
+    Pixel *data = not_uni.getDeepData();
 
     ASSERT_EQ(not_uni.getSize(), 6);
 
-    for(int i=0; i<6; i++)
+    for (int i = 0; i < 6; i++)
         EXPECT_EQ(data[i], tempData_not_uni[i]);
 
     delete data;
@@ -156,9 +168,9 @@ TEST_F(ImageTest, Flip90DX){
     delete data;
 }
 
-TEST_F(ImageTest, Flip90SX){
+TEST_F(ImageTest, Flip90SX) {
 
-    Pixel tempData_not_uni[3*2] = {
+    Pixel tempData_not_uni[3 * 2] = {
             Pixel(87, 42, 143, 255), Pixel(88, 255, 218, 255),
             Pixel(255, 0, 0, 255), Pixel(25, 61, 197, 255),
             Pixel(161, 255, 20, 255), Pixel(243, 94, 207, 255)};
@@ -166,11 +178,11 @@ TEST_F(ImageTest, Flip90SX){
     Pixel tempData_mono_green[1] = {Pixel(66, 133, 7, 255)};
 
     not_uni.flip90Sx();
-    Pixel* data = not_uni.getDeepData();
+    Pixel *data = not_uni.getDeepData();
 
     ASSERT_EQ(not_uni.getSize(), 6);
 
-    for(int i=0; i<6; i++)
+    for (int i = 0; i < 6; i++)
         EXPECT_EQ(data[i], tempData_not_uni[i]);
 
     delete data;
@@ -182,37 +194,152 @@ TEST_F(ImageTest, Flip90SX){
     delete data;
 }
 
-TEST_F(ImageTest, Contrast){
+TEST_F(ImageTest, Contrast) {
 
-    //
+    //#1
+    Pixel tempData_not_uni_1[2 * 3] = {
+            Pixel(177, 255, 0, 255), Pixel(255, 0, 0, 255), Pixel(66, 0, 150, 255),
+            Pixel(255, 77, 246, 255), Pixel(0, 27, 231, 255), Pixel(68, 255, 255, 255)
+    };
+
+    not_uni.contrast(20);
+
+    Pixel *data = not_uni.getDeepData();
+
+    ASSERT_EQ(not_uni.getSize(), 6);
+
+    for (int i = 0; i < 6; i++)
+        EXPECT_EQ(tempData_not_uni_1[i], data[i]);
+
+    delete data;
 }
 
-TEST_F(ImageTest, Brightness){
+TEST_F(ImageTest, ContrastOutOFRangePositive) {
+    //#2
+    Pixel tempData_not_uni_2[2 * 3] = {
+            Pixel(255, 255, 0, 255), Pixel(255, 0, 0, 255), Pixel(0, 0, 255, 255),
+            Pixel(255, 0, 255, 255), Pixel(0, 0, 255, 255), Pixel(0, 255, 255, 255)
+    };
 
-    //
+    not_uni.contrast(120);
+
+    Pixel *data = not_uni.getDeepData();
+
+    ASSERT_EQ(not_uni.getSize(), 6);
+
+    for (int i = 0; i < 6; i++)
+        EXPECT_EQ(tempData_not_uni_2[i], data[i]);
+
+    delete data;
 }
 
-TEST_F(ImageTest, Scale){
+TEST_F(ImageTest, ContrastOutOfRangeNegative) {
+    //#3
+    Pixel tempData_not_uni_3[2 * 3] = {
+            Pixel(128, 128, 128, 255), Pixel(128, 127, 127, 255), Pixel(128, 128, 128, 255),
+            Pixel(128, 128, 128, 255), Pixel(128, 128, 128, 255), Pixel(128, 128, 128, 255)
+    };
 
-    //
+    not_uni.contrast(-120);
+
+    Pixel *data = not_uni.getDeepData();
+
+    ASSERT_EQ(not_uni.getSize(), 6);
+
+    for (int i = 0; i < 6; i++)
+        EXPECT_EQ(tempData_not_uni_3[i], data[i]);
 }
 
-TEST_F(ImageTest, Blur){
+TEST_F(ImageTest, Brightness) {
 
-    Pixel tempData_not_unit[2*3] = {
+    Pixel tempData[2 * 3] = {
+            Pixel(255, 255, 147, 255), Pixel(255, 127, 127, 255), Pixel(214, 169, 255, 255),
+            Pixel(255, 221, 255, 255), Pixel(152, 188, 255, 255), Pixel(215, 255, 255, 255)
+    };
+
+    not_uni.brightness(50);
+    Pixel *data = not_uni.getDeepData();
+
+    ASSERT_EQ(not_uni.getSize(), 6);
+    for (int i = 0; i < 6; i++)
+        EXPECT_EQ(data[i], tempData[i]);
+}
+
+TEST_F(ImageTest, BrightnessOutOfRangePositive) {
+
+    Pixel tempData[2 * 3] = {
+            Pixel(255, 255, 255, 255), Pixel(255, 254, 254, 255), Pixel(255, 255, 255, 255),
+            Pixel(255, 255, 255, 255), Pixel(255, 255, 255, 255), Pixel(255, 255, 255, 255)
+    };
+
+    not_uni.brightness(120);
+    Pixel *data = not_uni.getDeepData();
+
+    ASSERT_EQ(not_uni.getSize(), 6);
+    for (int i = 0; i < 6; i++)
+        EXPECT_EQ(data[i], tempData[i]);
+}
+
+TEST_F(ImageTest, BrightnessOutOfRangeNegative) {
+
+    Pixel tempData[2 * 3] = {
+            Pixel(0, 0, 0, 255), Pixel(0, 0, 0, 255), Pixel(0, 0, 0, 255),
+            Pixel(0, 0, 0, 255), Pixel(0, 0, 0, 255), Pixel(0, 0, 0, 255)
+    };
+
+    not_uni.brightness(-120);
+    Pixel *data = not_uni.getDeepData();
+
+    ASSERT_EQ(not_uni.getSize(), 6);
+    for (int i = 0; i < 6; i++)
+        EXPECT_EQ(data[i], tempData[i]);
+}
+
+TEST_F(ImageTest, ScaleParametersDown) {
+
+    Pixel tempData[1 * 2] = {
+            Pixel(161, 255, 20, 255), Pixel(255, 0, 0, 255)
+    };
+
+    not_uni.scale(2, 1);
+    Pixel *data = not_uni.getDeepData();
+
+    ASSERT_EQ(not_uni.getSize(), 2);
+    for (int i = 0; i < 2; i++)
+        EXPECT_EQ(data[i], tempData[i]);
+}
+
+TEST_F(ImageTest, ScaleParametersUp) {
+
+    Pixel tempData[2 * 4] = {
+            Pixel(161, 255, 20, 255), Pixel(161, 255, 20, 255), Pixel(255, 0, 0, 255), Pixel(87, 42, 143, 255),
+            Pixel(243, 94, 207, 255), Pixel(243, 94, 207, 255), Pixel(25, 61, 197, 255), Pixel(88, 255, 218, 255)
+    };
+
+    not_uni.scale(4, 2);
+    Pixel *data = not_uni.getDeepData();
+
+    ASSERT_EQ(not_uni.getSize(), 8);
+    for (int i = 0; i < 8; i++)
+        EXPECT_EQ(data[i], tempData[i]);
+}
+
+TEST_F(ImageTest, Blur) {
+
+    Pixel tempData_not_unit[2 * 3] = {
             Pixel(180, 138, 101, 255), Pixel(146, 108, 102, 255), Pixel(112, 78, 129, 255),
             Pixel(174, 108, 138, 255), Pixel(130, 120, 153, 255), Pixel(86, 132, 168, 255)
     };
     Pixel tempData_mono_green[1] = {Pixel(63, 126, 28, 255)};
 
-    Pixel* data;
+    Pixel *data;
 
     not_uni.blur();
     data = not_uni.getDeepData();
 
-    ASSERT_EQ(not_uni.getSize(), 2*3);
+    ASSERT_EQ(not_uni.getSize(), 2 * 3);
 
-    for(int i=0; i<(2*3); i++)
+    for (int i = 0; i < (2 * 3); i++)
         EXPECT_EQ(data[i], tempData_not_unit[i]);
 
     delete data;
@@ -227,22 +354,22 @@ TEST_F(ImageTest, Blur){
     delete data;
 }
 
-TEST_F(ImageTest, EdgeDetect){
+TEST_F(ImageTest, EdgeDetect) {
 
-    Pixel tempData_not_unit[2*3] = {
+    Pixel tempData_not_unit[2 * 3] = {
             Pixel(0, 0, 0, 255), Pixel(176, 176, 176, 255), Pixel(154, 154, 154, 255),
             Pixel(14, 14, 14, 255), Pixel(214, 214, 214, 255), Pixel(0, 0, 0, 255)
     };
     Pixel tempData_mono_green[1] = {Pixel(0, 0, 0, 255)};
 
-    Pixel* data;
+    Pixel *data;
 
     not_uni.edgeDetect();
     data = not_uni.getDeepData();
 
-    ASSERT_EQ(not_uni.getSize(), 2*3);
+    ASSERT_EQ(not_uni.getSize(), 2 * 3);
 
-    for(int i=0; i<(2*3); i++)
+    for (int i = 0; i < (2 * 3); i++)
         EXPECT_EQ(data[i], tempData_not_unit[i]);
 
     delete data;
@@ -257,22 +384,22 @@ TEST_F(ImageTest, EdgeDetect){
     delete data;
 }
 
-TEST_F(ImageTest, Sharpen){
+TEST_F(ImageTest, Sharpen) {
 
-    Pixel tempData_not_unit[2*3] = {
+    Pixel tempData_not_unit[2 * 3] = {
             Pixel(0, 255, 0, 255), Pixel(255, 0, 0, 255), Pixel(0, 0, 225, 255),
             Pixel(255, 0, 255, 255), Pixel(0, 0, 255, 255), Pixel(49, 255, 255, 255)
     };
     Pixel tempData_mono_green[1] = {Pixel(66, 133, 0, 255)};
 
-    Pixel* data;
+    Pixel *data;
 
     not_uni.sharpen();
     data = not_uni.getDeepData();
 
-    ASSERT_EQ(not_uni.getSize(), 2*3);
+    ASSERT_EQ(not_uni.getSize(), 2 * 3);
 
-    for(int i=0; i<(2*3); i++)
+    for (int i = 0; i < (2 * 3); i++)
         EXPECT_EQ(data[i], tempData_not_unit[i]);
 
     delete data;
@@ -287,22 +414,22 @@ TEST_F(ImageTest, Sharpen){
     delete data;
 }
 
-TEST_F(ImageTest, Emboss){
+TEST_F(ImageTest, Emboss) {
 
-    Pixel tempData_not_unit[2*3] = {
+    Pixel tempData_not_unit[2 * 3] = {
             Pixel(65, 0, 71, 255), Pixel(0, 0, 255, 255), Pixel(0, 255, 255, 255),
             Pixel(0, 0, 255, 255), Pixel(0, 255, 255, 255), Pixel(0, 255, 255, 255)
     };
     Pixel tempData_mono_green[1] = {Pixel(66, 133, 0, 255)};
 
-    Pixel* data;
+    Pixel *data;
 
     not_uni.emboss();
     data = not_uni.getDeepData();
 
-    ASSERT_EQ(not_uni.getSize(), 2*3);
+    ASSERT_EQ(not_uni.getSize(), 2 * 3);
 
-    for(int i=0; i<(2*3); i++)
+    for (int i = 0; i < (2 * 3); i++)
         EXPECT_EQ(data[i], tempData_not_unit[i]);
 
     delete data;
