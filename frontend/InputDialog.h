034c91d72a342865cb8a070df663319bcf440d52
Jacopo Zecchi <jacopo.zecchi@gmail.com>
2021-06-14 19:10:54 +0200

    Constructor is now private

--- frontend/InputDialog.h
+++ frontend/InputDialog.h
@@ -10,11 +10,11 @@ class InputDialog : public QDialog
 {
     Q_OBJECT
 public:
-    explicit InputDialog(const QList<QString>& fields, int min=0, int max=100, int step=1, QWidget *parent = nullptr);
 
     static QList<int> getFields(QWidget *parent, QList<QString> fields, int min, int max, int step, bool *ok);
 
 private:
+    explicit InputDialog(const QList<QString>& fields, int min=0, int max=100, int step=1, QWidget *parent = nullptr);
     QList<QSpinBox* > values;
     QList<QString> fields;
     int min;
